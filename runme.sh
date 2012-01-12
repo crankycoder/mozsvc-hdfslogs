2c9a50b56f69ae02ea7bea18cd1129676196be67
Victor Ng <crankycoder@gmail.com>
2012-01-12 16:49:01 -0500

    updated runme script for the explcit hadoop-site.xml path

--- runme.sh
+++ runme.sh
@@ -1,5 +1,5 @@
 # Bizarrely, you need to have the hadoop configuration directory in
 # your CLASSPATH or else Hadoop won't find your DFS configuration
 export HADOOP_HOME="/Users/victorng/dev/hadoop-0.20.203.0/"
-export CLASSPATH="./lib/*:dist/lib/*:/Users/victorng/dev/hadoop-0.20.203.0/conf"
-/usr/bin/env java com.mozilla.services.hdfs.LogReader -i fixtures/sample.json.log -hdfs_path /hdfs/var/log/metlog
+export CLASSPATH="./lib/*:dist/lib/*" 
+/usr/bin/env java com.mozilla.services.hdfs.LogReader -i fixtures/sample.json.log -hdfs_path /hdfs/var/log/metlog -sitexml conf/hadoop-site.xml -output_dir /tmp/done
